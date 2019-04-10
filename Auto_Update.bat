2db0ca7ce551ac44c6e016fd083471329deee8ad
mer0llac0aster <kmerolla@gmail.com>
2019-04-10 17:31:06 -0400

    Update Auto_Update.bat
    updated download URL

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,6 +1,6 @@
 
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('http://sysmonchart.azurewebsites.net/sysmonconfig-server.xml','C:\ProgramData\sysmon\sysmonconfig-server.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mer0llac0aster/jubilant-giggle/master/sysmonconfig-server.xml','C:\ProgramData\sysmon\sysmonconfig-server.xml')"
 sysmon64 -c sysmonconfig-config.xml
 exit
