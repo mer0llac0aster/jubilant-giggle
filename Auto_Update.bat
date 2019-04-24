090212a60597abaf8167e9de2cb4e26c9d71ba69
mer0llac0aster <kmerolla@gmail.com>
2019-04-24 10:42:12 -0400

    Update Auto_Update.bat
    removed exit

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -3,4 +3,3 @@
 cd C:\ProgramData\sysmon\
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mer0llac0aster/jubilant-giggle/master/sysmonconfig-server.xml','C:\ProgramData\sysmon\sysmonconfig-server.xml')"
 sysmon64 -c sysmonconfig-config.xml
-exit
